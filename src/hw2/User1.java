ab8546fd6919314ce630cd163258a3737a3772ba
NicoleKio <nastiamay.1654@gmail.com>
2024-12-05 22:56:24 +0200

    new 2 hw ac created

---
+++ src/hw2/User1.java
@@ -0,0 +1,13 @@
+package hw2;
+
+
+public class User1 {
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+
+    private class Addres{
+
+    }
+}
