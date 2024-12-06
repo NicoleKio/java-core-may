c92dd1498672d1f48458ba18bf1a7437d59646b6
NicoleKio <nastiamay.1654@gmail.com>
2024-12-06 20:48:39 +0200

    created User1 from hw2 // need to fix inner classes

--- src/hw2/Main2.java
+++ src/hw2/Main2.java
@@ -3,6 +3,35 @@ package hw2;
 public class Main2 {
     public static void main(String[] args) {
 
+        User1 user1 = User1.builder()
+                .id(1)
+                .name("yuu")
+                .username("yuu-chan")
+                .email("yuu@gmail.com")
+                .build();
+        User1.Address address = user1.new Address(
+                "y1",
+                "i1",
+                "Odessa",
+                "44454",
+                new User1.Address.Geo("ooo", "ppp")
+                );
+
+        //        User1.Address.Geo geo = User1.Address.Geo.builder()
+        //                .lat("40.7128")
+        //                .lng("-74.0060")
+        //                .build()
+        user1.setAddress(address);
+
+        User1.Company company = User1.Company.builder()
+                .name("Nana")
+                .catchPhrase("Just Do")
+                .bs("444")
+                .build();
+        user1.setCompany(company);
+
+        System.out.println(user1);
+
 
     }
 }
--- src/hw2/User1.java
+++ src/hw2/User1.java
@@ -1,13 +1,44 @@
 package hw2;
 
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class User1 {
     private int id;
     private String name;
     private String username;
     private String email;
 
-    private class Addres{
+    @Data
+    @Builder
+    public class Address{
+        private String street;
+        private String suite;
+        private String city;
+        private String zipcode;
 
+        @Data
+        @Builder
+        public static class Geo{
+            private String lat;
+            private String lng;
+        }
+
+        private Geo geo;
     }
+
+    private Address address;
+    private int phone;
+    private String website;
+
+    @Data
+    @Builder
+    public static class Company{
+        private String name;
+        private String catchPhrase;
+        private String bs;
+    }
+    private Company company;
 }
